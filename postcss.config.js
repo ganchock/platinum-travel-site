23f84f4dcc3fc33c36466089fe84ff4fbfa68b96
ganchock <ganchock@yahoo.com>
2022-03-05 19:18:39 +0800

    header is completed

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
         require("postcss-import"),
         require("postcss-mixins"),
         require("postcss-nested"),
+        require("postcss-hexrgba"),
         require("postcss-simple-vars"),
         require("autoprefixer"),
         require("postcss-preset-env"),
