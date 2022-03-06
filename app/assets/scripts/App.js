92b111110f31f00fe28daf92861c10882fa325f5
ganchock <ganchock@yahoo.com>
2022-03-06 20:09:29 +0800

    Javascript deer scroll hiij tohiruulav.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import css from "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
 let mobileMenu = new MobileMenu();
 if (module.hot) {
     module.hot.accept();
