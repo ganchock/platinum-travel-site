a5cdfacb2e641a9df9631ec4863caa1dde37019f
ganchock <ganchock@yahoo.com>
2022-03-07 00:17:37 +0800

    Modal overlay-ийг бүрэн хийж дууслаа

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,27 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 
-let stickyHeader = new StickyHeader();
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
-let mobileMenu = new MobileMenu();
+new MobileMenu();
+let modal;
+
+document.querySelectorAll(".open-modal").forEach((el) => {
+    el.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (typeof modal == "undefined") {
+            import ("./modules/Modal")
+            .then((x) => {
+                    modal = new x.default();
+                    setTimeout(() => modal.openModal(), 20);
+                })
+                .catch(() => console.log("Файлыг дуудахад алдаа гарлаа."));
+        } else {
+            modal.openModal();
+        }
+    });
+});
 if (module.hot) {
     module.hot.accept();
 }
