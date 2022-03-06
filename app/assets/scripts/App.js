62b23aca1b6994ad050212e8a55ab92dacb4d015
ganchock <ganchock@yahoo.com>
2022-03-06 22:42:37 +0800

    Sticky header хийж дуусгав.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import css from "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
+let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
 let mobileMenu = new MobileMenu();
